ca77950e3960adcc4376ec2bc625dea775f8e659
bojesomo <positivegenius4real@yahoo.com>
2022-03-17 09:32:24 +0400

    Update __init__.py

--- hypercomplex/__init__.py
+++ hypercomplex/__init__.py
@@ -1,7 +1,7 @@
 from .hypercomplex_layers import (ComplexConv1D, ComplexConv2D, ComplexConv3D, ComplexLinear,
                                   QuaternionConv1D, QuaternionConv2D, QuaternionConv3D, QuaternionLinear,
                                   OctonionConv1D, OctonionConv2D, OctonionConv3D, OctonionLinear,
-                                  SedanionConv1D, SedanionConv2D, SedanionConv3D, SedanionLinear,
+                                  SedenionConv1D, SedenionConv2D, SedenionConv3D, SedenionLinear,
                                   HyperConv1D, HyperConv2D, HyperConv3D, HyperLinear)
 
 from .hypercomplex_ops import get_c
